6af4c9fa292b2bfb626ac60a441bb1361fc42534
ExtranoDev <jeomade19@gmail.com>
2023-02-23 23:40:52 +0000

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -11,5 +11,5 @@ def index_range(page, page_size):
             page_size(int): page size
     """
     start_idx = (page - 1) * page_size
-    end_idx = start_idx * page_size
+    end_idx = start_idx + page_size
     return start_idx, end_idx
