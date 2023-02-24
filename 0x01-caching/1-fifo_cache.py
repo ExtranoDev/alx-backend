e53141149121f61901d72ab67bc3d13b64ed8957
ExtranoDev <jeomade19@gmail.com>
2023-02-24 23:51:43 +0000

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""FIFO caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache that inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            key = list(self.cache_data.items())[0][0]
+            del self.cache_data[key]
+            print("DISCARD: {}".format(key))
+
+    def get(self, key):
+        """return a value linked to the key"""
+        data = self.cache_data.get(key)
+        return data if data is not None else None
