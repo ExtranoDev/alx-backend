5bf55bac7426fe9a523b405a53ddd15adc9fb438
ExtranoDev <jeomade19@gmail.com>
2023-03-31 14:58:55 +0000

    Redis async opperation

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,26 @@
+import {createClient, print} from 'redis';
+
+const client = createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', function(err) {
+  console.log('Redis client not connected to the server: ' + err)
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value);
+  print("Value set");
+}
+
+async function displaySchoolValue(schoolName) {
+  await client.get(schoolName, (err, val) => {
+    console.log(`${val}`);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
